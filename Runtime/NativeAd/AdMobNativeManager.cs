2204adf9cb0380e700a6efd95199b5dbd10912e7
Rd05 <rd05@Rd05deMac-Studio.local>
2024-09-11 16:01:05 +0800

    wip: AdMob修到一半

--- Runtime/NativeAd/AdMobNativeManager.cs
+++ Runtime/NativeAd/AdMobNativeManager.cs
@@ -1,8 +1,7 @@
-﻿#if AdFactory_Admob_Native
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using GoogleMobileAds.Api;
+using GoogleMobileAds.Api.AdManager;
 using UnityEngine;
 
 public class AdMobNativeManager
@@ -20,37 +19,54 @@ public class AdMobNativeManager
         }
     }
 
+    private NativeAd nativeAd;
 
-    public void SetUpNativeAd(string AD_UNIT, Action<UnifiedNativeAd> OnLoadSuccess, Action OnLoadFaild, Dictionary<string, string> extras = null)
+    /// <summary>
+    /// 設置原生廣告
+    /// </summary>
+    /// <param name="AD_UNIT">廣告單元 ID</param>
+    /// <param name="OnLoadSuccess">成功加載後的回調</param>
+    /// <param name="OnLoadFaild">加載失敗的回調</param>
+    /// <param name="extras">其他附加參數</param>
+    public void SetUpNativeAd(string AD_UNIT, Action<NativeAd> OnLoadSuccess, Action OnLoadFaild, Dictionary<string, string> extras = null)
     {
         var adLoader = new AdLoader.Builder(AD_UNIT)
-        .ForUnifiedNativeAd()
-        .Build();
+            .ForNativeAd()
+            .Build();
 
-        adLoader.OnUnifiedNativeAdLoaded += delegate (object sender, UnifiedNativeAdEventArgs e)
+        // 加載成功處理
+        adLoader.OnNativeAdLoaded += delegate (object sender, NativeAdEventArgs e)
         {
-            //Debug.LogError("NativeAd Loaded : " + AD_UNIT);
-            OnLoadSuccess(e.nativeAd);
+            nativeAd = e.nativeAd;
+            Debug.Log("Native Ad Loaded: " + AD_UNIT);
+            OnLoadSuccess?.Invoke(nativeAd);
         };
 
+        // 加載失敗處理
         adLoader.OnAdFailedToLoad += delegate (object sender, AdFailedToLoadEventArgs e)
         {
-            //Debug.LogError("NativeAd OnAdFailedToLoad : " + AD_UNIT + " ,msg: " + e.Message);
-            OnLoadFaild();
+            Debug.LogError("Native Ad Failed to Load: " + AD_UNIT + " ,msg: " + e.LoadAdError.GetMessage());
+            OnLoadFaild?.Invoke();
         };
 
-        var adRequest = new AdRequest.Builder();
+        var adRequest = new AdRequest();
         if (extras != null)
         {
-            foreach (var item in extras)
-            {
-                adRequest.AddExtra(item.Key, item.Value);
-            }
+            adRequest.Extras = extras;
         }
 
-        adLoader.LoadAd(adRequest.Build());
-
+        // 開始加載廣告
+        adLoader.LoadAd(adRequest);
     }
 
+    /// <summary>
+    /// 銷毀已加載的廣告
+    /// </summary>
+    public void Destroy()
+    {
+        if (nativeAd != null)
+        {
+            nativeAd.Destroy();
+        }
+    }
 }
-#endif
--- Runtime/NativeAd/NativeAdFactory.cs
+++ Runtime/NativeAd/NativeAdFactory.cs
@@ -1,26 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Api.AdManager;
 using UnityEngine;
 
 public class NativeAdFactory : MonoBehaviour
 {
-
     INativeAdManager nativeAdManager;
 
     /// <summary>
-    /// 初始化 AdFactory 並指定實做的廣告供應者
+    /// 初始化 NativeAdFactory 並指定實做的廣告供應者
     /// </summary>
-    public void Init(string AD_UNIT)
+    public void Init(INativeAdManager adManager, string AD_UNIT)
     {
+        nativeAdManager = adManager;
         nativeAdManager.Init(AD_UNIT);
     }
 
+    public void LoadAd(string AD_UNIT)
+    {
+        if (nativeAdManager != null)
+        {
+            nativeAdManager.LoadNativeAd(AD_UNIT);
+        }
+    }
 
+    public void DestroyAd()
+    {
+        if (nativeAdManager != null)
+        {
+            nativeAdManager.Destroy();
+        }
+    }
 }
 
 public interface INativeAdManager
 {
     void Init(string AD_UNIT);
+    void LoadNativeAd(string AD_UNIT);
     void Destroy();
 }
 
